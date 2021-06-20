18be3cc381f9afb8b38ad846ede8fd49e96bfdd2
Ugur_ml97 <Ugur_ml97@itstep.edu.az>
2021-06-21 02:51:33 +0400

    Добавьте файлы проекта.

---
+++ winform 3-1/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace winform_3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nameupdatetxtb = new System.Windows.Forms.TextBox();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Humans = new System.Windows.Forms.ListBox();
+            this.loadbtn = new System.Windows.Forms.Button();
+            this.Savebtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.nameupdatetxtb);
+            this.groupBox1.Controls.Add(this.UpdateBtn);
+            this.groupBox1.Controls.Add(this.Addbtn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.groupBox1.Location = new System.Drawing.Point(13, 40);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(704, 451);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Anket";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(299, 370);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(374, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Enter the name of the person you want to update";
+            // 
+            // nameupdatetxtb
+            // 
+            this.nameupdatetxtb.Location = new System.Drawing.Point(409, 406);
+            this.nameupdatetxtb.Multiline = true;
+            this.nameupdatetxtb.Name = "nameupdatetxtb";
+            this.nameupdatetxtb.Size = new System.Drawing.Size(155, 34);
+            this.nameupdatetxtb.TabIndex = 13;
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.AutoSize = true;
+            this.UpdateBtn.BackColor = System.Drawing.SystemColors.Info;
+            this.UpdateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UpdateBtn.Location = new System.Drawing.Point(39, 370);
+            this.UpdateBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(100, 50);
+            this.UpdateBtn.TabIndex = 12;
+            this.UpdateBtn.Text = "Update";
+            this.UpdateBtn.UseVisualStyleBackColor = false;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.BackColor = System.Drawing.SystemColors.Info;
+            this.Addbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Addbtn.Location = new System.Drawing.Point(39, 315);
+            this.Addbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(100, 50);
+            this.Addbtn.TabIndex = 11;
+            this.Addbtn.Text = "Add";
+            this.Addbtn.UseVisualStyleBackColor = false;
+            this.Addbtn.Click += new System.EventHandler(this.Addbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.SystemColors.Info;
+            this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.SystemColors.Info;
+            this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.dateTimePicker1.CalendarTrailingForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.dateTimePicker1.Location = new System.Drawing.Point(129, 243);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox4.Location = new System.Drawing.Point(174, 111);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(164, 40);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Surname";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(20, 177);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Email";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Location = new System.Drawing.Point(172, 161);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(164, 41);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(17, 243);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Birth date";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(172, 64);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 39);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(17, 122);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PhoneNumber";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(172, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 40);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Humans
+            // 
+            this.Humans.BackColor = System.Drawing.Color.LemonChiffon;
+            this.Humans.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Humans.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.Humans.FormattingEnabled = true;
+            this.Humans.ItemHeight = 20;
+            this.Humans.Location = new System.Drawing.Point(725, 43);
+            this.Humans.Margin = new System.Windows.Forms.Padding(4);
+            this.Humans.Name = "Humans";
+            this.Humans.Size = new System.Drawing.Size(312, 144);
+            this.Humans.TabIndex = 14;
+            // 
+            // loadbtn
+            // 
+            this.loadbtn.BackColor = System.Drawing.SystemColors.Info;
+            this.loadbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadbtn.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.loadbtn.Location = new System.Drawing.Point(748, 420);
+            this.loadbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.loadbtn.Name = "loadbtn";
+            this.loadbtn.Size = new System.Drawing.Size(100, 50);
+            this.loadbtn.TabIndex = 15;
+            this.loadbtn.Text = "LOAD";
+            this.loadbtn.UseVisualStyleBackColor = false;
+            this.loadbtn.Click += new System.EventHandler(this.loadbtn_Click);
+            // 
+            // Savebtn
+            // 
+            this.Savebtn.BackColor = System.Drawing.SystemColors.Info;
+            this.Savebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Savebtn.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.Savebtn.Location = new System.Drawing.Point(893, 420);
+            this.Savebtn.Margin = new System.Windows.Forms.Padding(4);
+            this.Savebtn.Name = "Savebtn";
+            this.Savebtn.Size = new System.Drawing.Size(100, 50);
+            this.Savebtn.TabIndex = 16;
+            this.Savebtn.Text = "SAVE";
+            this.Savebtn.UseVisualStyleBackColor = false;
+            this.Savebtn.Click += new System.EventHandler(this.Savebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::winform_3_1.Properties.Resources.Margo;
+            this.ClientSize = new System.Drawing.Size(1051, 559);
+            this.Controls.Add(this.Savebtn);
+            this.Controls.Add(this.loadbtn);
+            this.Controls.Add(this.Humans);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox nameupdatetxtb;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.Button Addbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox Humans;
+        private System.Windows.Forms.Button loadbtn;
+        private System.Windows.Forms.Button Savebtn;
+    }
+}
+
---
+++ winform 3-1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            nameupdatetxtb.Enabled = false;
+
+        }
+
+        private void Addbtn_Click(object sender, EventArgs e)
+        {
+            this.UpdateBtn.Location = new Point(30, 300);
+            this.Addbtn.Location = new Point(30, 250);
+            nameupdatetxtb.Enabled = false;
+            Human human = new Human();
+            human.Name = textBox1.Text;
+            human.Surname = textBox2.Text;
+            human.Email = textBox4.Text;
+            human.BirthDate = dateTimePicker1.Text;
+            Humans.Items.Add(human);
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            //   this.UpdateBtn.Top  = 250;
+            //     this.Addbtn .up
+            this.UpdateBtn.Location = new Point(30, 250);
+            this.Addbtn.Location = new Point(30, 300);
+            try
+            {
+                nameupdatetxtb.Enabled = true;
+
+                foreach (Human item in Humans.Items)
+                {
+
+                    if (item.Name == nameupdatetxtb.Text)
+                    {
+                        Humans.Items.Remove(item);
+                        Human human = new Human();
+                        human.Name = textBox1.Text;
+                        human.Surname = textBox2.Text;
+                        human.Email = textBox4.Text;
+                        human.BirthDate = dateTimePicker1.Text;
+                        Humans.Items.Add(human);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+        }
+
+        private void Savebtn_Click(object sender, EventArgs e)
+        {
+            Human human = new Human();
+            human.Name = textBox1.Text;
+            human.Surname = textBox2.Text;
+            human.Email = textBox4.Text;
+            human.BirthDate = dateTimePicker1.Text;
+
+            List<Human> humans = new List<Human>
+            {
+                new Human
+                {
+                    Email ="ZeynebHesenova@gmail.com",
+                    Name="zeyneb",
+                    Surname ="hesenova",
+                    PhoneNumber ="12312323535",
+
+                },
+
+                new Human
+                {
+Name ="fuad",
+Surname ="ugurlu",
+Email ="fuad123@gmail.com",
+PhoneNumber ="+12235256"
+                },
+                new Human
+                {
+                    Name ="nezrin",
+                    Surname ="rehimli",
+                    Email="nezrin123@gmail.com",
+                    PhoneNumber ="+9941312334"
+                }
+
+
+
+        };
+            humans.Add(human);
+
+
+            var serializer = new JsonSerializer();
+            using (var sw = new StreamWriter("humans.json"))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    serializer.Serialize(jw, humans);
+                }
+            }
+        }
+
+        private void loadbtn_Click(object sender, EventArgs e)
+        {
+            Human[] humans = null;
+            var serializer = new JsonSerializer();
+            using (var sr = new StreamReader("humans.json"))
+            {
+                using (var jr = new JsonTextReader(sr))
+                {
+                    humans = serializer.Deserialize<Human[]>(jr);
+                }
+                foreach (var item in humans)
+                {
+                    Humans.Items.Add(item);
+                }
+                Human human = new Human();
+                human.Name = textBox1.Text;
+                human.Surname = textBox2.Text;
+                human.Email = textBox4.Text;
+                human.BirthDate = dateTimePicker1.Text;
+                Humans.Items.Add(human);
+            }
+        }
+    }
+
+}
+    
